09a6bef17808d75100fcca8b19f0a146ea4e4d87
Eloenk <64978907+Eloenk@users.noreply.github.com>
2024-05-21 18:40:54 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+// Contains the default configuration for Bot & Plugins
+// Any attribute not given in the configuration will take its default value
+
+const botConfig = {
+  authFolder: "auth",
+  selfReply: false,
+  logMessages: true,
+};
+
+const pluginsConfig = {
+  mirror: {
+    prefix: "!mirror!",
+  },
+  roles: {
+    dataFile: "./roles.json",
+    prefix: "!role ",
+    updateOnAdd: false,
+    updateOnRemove: false,
+  },
+  tagEveryone: {
+    membersLimit: 1024,
+    trigger: ".tag",
+    botta: "2347082282068",
+    dem: "2348149149691",
+  },
+};
+
+module.exports = { botConfig, pluginsConfig };
